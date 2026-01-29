cb588d21b1fcf61470d1f36944581d258787c2c8
Capretta1 <blessnutsua3@gmail,com>
2026-01-29 22:53:32 +0000

    implemented rio_writen function and some changes to rio_nread function

--- rioPackage.c
+++ rioPackage.c
@@ -2,7 +2,7 @@
 
 
 /*
-*The rio_readn function transfers up to n bytes from the-current file position
+*This function transfers up to n bytes from the-current file position
 *of descriptor fd into memory location usrbuf
 */
 ssize_t rio_readn(int fd, void *usrbuf, size_t n)
@@ -14,7 +14,7 @@ ssize_t rio_readn(int fd, void *usrbuf, size_t n)
   while(nleft > 0){
 	if((nread = read(fd, bufp, nleft)) < 0){
 		if(errno == EINTR)   /*Interrupted by sig handler return*/
-			nread = 0;  /*and call(), and read() again */
+			nread = 0;  /*and call read() again */
 		  else
 			return -1;  /* err no set by read() */
 }
@@ -24,4 +24,28 @@ nleft -= nread;
 bufp += nread;
 }
 return (n - nleft); 	  	  /*Return >= 0 */
+}
+
+
+
+/*
+*This function transfers n bytes from location ursbuf to descriptor fd
+*/
+ssize_t rio_writen(int fd, void *ursbuf, size_t n)
+{
+ size_t nleft = n;
+ ssize_t nwritten;
+ char *bufp = ursbuf;
+
+ while (nleft > 0){
+	if((nwritten = write(fd, bufp, nleft) <= 0){
+		if (errno = ENTR)        /*Interruptes by sig handler return*/
+			nwritten = 0;   /* and call write() again*/
+		else
+			return -1;     /* err no set by write() */
+}
+nleft -= nwritten;
+bufp += nwritten;
+}
+return n;
 }
