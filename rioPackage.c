cb4d67e2333883c2702cb063671d379fd0f5820e
Capretta1 <blessnutsua3@gmail,com>
2026-01-30 11:55:55 +0000

    added the function rio_readinitb

--- rioPackage.c
+++ rioPackage.c
@@ -49,3 +49,14 @@ bufp += nwritten;
 }
 return n;
 }
+
+
+/*
+*This function is called once per open descriptor. It associates the descriptor fd with a read buffer of type rio_t at address rp.
+*/
+void rio_readinitb(rio_t *rp, int fd)
+{
+	rp->rio_fd = fd;
+	rp->rio_cnt = 0;
+	rp->rio_bufptr = rp->rio_buf;
+}
